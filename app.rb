1a7cb861d8fd4e9accfd6d2cac0e043951c78bbf
Rick <dirklo@gmail.com>
2021-01-20 11:13:37 +0100

    commit tree 58d4df4d14a5249bfee519640fc072e0ee4141d9

--- app.rb
+++ app.rb
@@ -1,6 +1,31 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get "/reversename/:name" do
+    "#{params[:name].reverse}"
+  end
 
+  get "/square/:number" do
+    (params[:number].to_i**2).to_s
+  end
+
+  get "/say/:number/:phrase" do
+    "#{params[:phrase] * params[:number].to_i}"
+  end
+  
+  get "/say/:word1/:word2/:word3/:word4/:word5" do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do
+    operations = {
+      "add"=>"+",
+      "subtract"=>"-",
+      "multiply"=>"*",
+      "divide"=>"/"
+    }
+
+    params[:number1].to_i.send("#{operations[params[:operation]]}", params[:number2].to_i).to_s
+
+  end
 end
